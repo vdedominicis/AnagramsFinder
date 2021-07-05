1427f3a27048af9b47270c40c6724d17f8f49a41
Valerio De Dominicis <ded.valerio@hotmail.it>
2021-07-05 13:20:13 +0200

    removed useless import

--- src/test/java/org/apache/maven/anagrams/FileUtilsTest.java
+++ src/test/java/org/apache/maven/anagrams/FileUtilsTest.java
@@ -2,7 +2,6 @@ package org.apache.maven.anagrams;
 
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
